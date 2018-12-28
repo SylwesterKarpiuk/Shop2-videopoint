22caf9b75cab705c490e8c1388b6dd10bceca45b
Sylwester Karpiuk <sylwesterkarpiuk@gmail.com>
2018-12-28 13:28:11 +0100

    Dodaj pliki projektów.

---
+++ Shop2 videopoint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shop2_videopoint.Startup))]
+namespace Shop2_videopoint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
